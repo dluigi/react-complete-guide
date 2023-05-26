1eca2e9caf155dca57ec7516639ed9fbff1a1b99
dluigi <luigi.d.smmc@gmail.com>
2023-05-26 14:03:41 +0300

    Test Commit between Laptop and PC

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const expenses = [
     {
-      id: "e1",
+      id: "e0",
       title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
